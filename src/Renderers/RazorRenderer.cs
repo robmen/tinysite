35bbcaa63da448894aad7cf8b661628d31b70c48
Rob Mensching <rob@robmensching.com>
2014-05-02 10:09:45 -0700

    Add RazorRenderer and move to NETFX v4.5 because it requires it.

---
+++ src/Renderers/RazorRenderer.cs
@@ -0,0 +1,19 @@
+﻿using RazorEngine;
+using TinySite.Rendering;
+
+namespace TinySite.Renderers
+{
+    [Render("cshtml")]
+    [Render("razor")]
+    public class RazorRenderer : IRenderer
+    {
+        public string Render(string template, object data)
+        {
+            var cacheName = template.GetHashCode().ToString();
+
+            var result = Razor.Parse(template, data, cacheName);
+
+            return result;
+        }
+    }
+}
