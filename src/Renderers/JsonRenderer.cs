631c0329e44450a79d1821115f23a7bfbde175cd
Rob Mensching <rob@robmensching.com>
2015-02-06 01:12:11 -0800

    Add support for "rendering" .json files.
    JSON content can now be used to populate the metadata and content of a
    document. This allows for data to be rendered into some final output.

---
+++ src/Renderers/JsonRenderer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using TinySite.Rendering;
+
+namespace TinySite.Renderers
+{
+    [Render("json")]
+    public class JsonRenderer : IRenderer
+    {
+        private object sync = new object();
+
+        public string Render(string path, string template, dynamic data)
+        {
+            var document = data.Document;
+
+            var content = String.Empty;
+
+            lock (sync)
+            {
+                foreach (var token in JObject.Parse(template))
+                {
+                    if (token.Key.Equals("Content", StringComparison.OrdinalIgnoreCase))
+                    {
+                        content = (string)token.Value;
+                    }
+                    else
+                    {
+                        document[token.Key] = (string)token.Value;
+                    }
+                }
+            }
+
+            return content;
+        }
+    }
+}
