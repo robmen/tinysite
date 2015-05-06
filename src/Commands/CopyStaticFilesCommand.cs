0e112bd71f9f2ff1b1eb7b19d2238e4ab4f30979
Rob Mensching <rob@robmensching.com>
2015-05-06 00:05:51 -0700

    #ifdef out unused code.

--- src/Commands/CopyStaticFilesCommand.cs
+++ src/Commands/CopyStaticFilesCommand.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using TinySite.Models;
 
 namespace TinySite.Commands
@@ -19,6 +18,7 @@ namespace TinySite.Commands
                 .Count();
         }
 
+#if false
         public async Task<int> ExecuteAsync()
         {
             var streams = new List<Stream>(this.Files.Count() * 2);
@@ -60,6 +60,7 @@ namespace TinySite.Commands
 
             return this.CopiedFiles;
         }
+#endif
 
         private static StaticFile CopyStaticFile(StaticFile file)
         {
