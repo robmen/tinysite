083faccbe16a325487c2288a69b4082c2ce87650
Rob Mensching <rob@robmensching.com>
2016-01-04 02:40:59 -0800

    Use CommonMark.NET instead of MarkdownDeep.

--- src/Renderers/MarkdownRenderer.cs
+++ src/Renderers/MarkdownRenderer.cs
@@ -1,5 +1,5 @@
 ﻿using System.Collections.Generic;
-using MarkdownDeep;
+using CommonMark;
 using TinySite.Models;
 using TinySite.Rendering;
 
@@ -11,21 +11,13 @@ namespace TinySite.Renderers
     {
         public MarkdownRenderer()
         {
-            this.MarkdownEngine = new Markdown();
-            this.MarkdownEngine.ExtraMode = true;
-            this.MarkdownEngine.SafeMode = false;
         }
-
-        private Markdown MarkdownEngine { get; set; }
 
         public string Render(SourceFile sourceFile, string template, object data)
         {
-            lock (this.MarkdownEngine)
-            {
-                var result = this.MarkdownEngine.Transform(template).Trim();
-
-                return result;
-            }
+            var result = CommonMarkConverter.Convert(template);
+            
+            return result.Trim();
         }
 
         public void Unload(IEnumerable<string> paths)
