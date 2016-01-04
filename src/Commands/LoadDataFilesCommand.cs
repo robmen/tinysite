1384a63405148875f82bfdf030ce93ed6d77a8d8
Rob Mensching <rob@robmensching.com>
2016-01-04 02:11:34 -0800

    Allow data files to get their date from their filename like documents.

--- src/Commands/LoadDataFilesCommand.cs
+++ src/Commands/LoadDataFilesCommand.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace TinySite.Commands
 {
     public class LoadDataFilesCommand
     {
+        private static readonly Regex DateFromFileName = new Regex(@"^\s*(?<year>\d{4})-(?<month>\d{1,2})-(?<day>\d{1,2})([Tt@](?<hour>\d{1,2})\.(?<minute>\d{1,2})(\.(?<second>\d{1,2}))?)?[-\s]\s*", RegexOptions.CultureInvariant | RegexOptions.ExplicitCapture | RegexOptions.Singleline);
+
         public LoadDataFilesCommand(string dataPath, IEnumerable<AdditionalMetadataConfig> additionalMetadata, IEnumerable<Regex> ignoreFiles)
         {
             this.DataPath = dataPath;
@@ -66,6 +69,8 @@ namespace TinySite.Commands
             var parser = new ParseDocumentCommand(path);
             await parser.ExecuteAsync();
 
+            var metadataDate = parser.Date;
+
             if (this.AdditionalMetadataForFiles != null)
             {
                 var rootPath = Path.GetDirectoryName(this.DataPath.TrimEnd('\\'));
@@ -80,11 +85,33 @@ namespace TinySite.Commands
                 }
             }
 
+            var disableDateFromFileName = parser.Metadata.GetAndRemove("DisableDateFromFileName", false);
+
+            // If the parser metadata didn't specify the date, try to use a date from the filename.
+            //
+            if (!disableDateFromFileName && !metadataDate.HasValue)
+            {
+                var fileName = Path.GetFileName(path);
+                var match = DateFromFileName.Match(fileName);
+
+                if (match.Success)
+                {
+                    var year = Convert.ToInt32(match.Groups[1].Value, 10);
+                    var month = Convert.ToInt32(match.Groups[2].Value, 10);
+                    var day = Convert.ToInt32(match.Groups[3].Value, 10);
+                    var hour = match.Groups[4].Success ? Convert.ToInt32(match.Groups[4].Value, 10) : 0;
+                    var minute = match.Groups[5].Success ? Convert.ToInt32(match.Groups[5].Value, 10) : 0;
+                    var second = match.Groups[6].Success ? Convert.ToInt32(match.Groups[6].Value, 10) : 0;
+
+                    metadataDate = new DateTime(year, month, day, hour, minute, second);
+                }
+            }
+
             var dataFile = new DataFile(path, this.DataPath, parser.Content, parser.Metadata, parser.Queries);
 
-            if (parser.Date.HasValue)
+            if (metadataDate.HasValue)
             {
-                dataFile.SetTimes(parser.Date.Value);
+                dataFile.SetTimes(metadataDate.Value);
             }
 
             return dataFile;
